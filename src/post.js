b5478358098e9e05536504e0c7e0370589f6e497
Arjansho1~ <arjansho@amazon.de>
2023-07-05 18:19:24 +0200

    Commit Js Project +1 Action

--- src/post.js
+++ src/post.js
@@ -1,75 +1,92 @@
-const GRAPHQL_POST_API_BASE_URL ="//";
+window.addEventListener("load", async () => {
+    try {
+        const response = await fetch(
+            "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
+        );
+        const data = await response.json();
 
-export async function getPostById(postId: number) {
-  const response = await fetch(GRAPHQL_POST_API_BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: `query ($postId: Int!) {
-        post(id: $postId) {
-          id
-          title
-          description
-          body
-          cover,
-          coverDescription,
-          createdAt
+        const mainProjectId = getQuerystringId();
+        let mainProjectIndex = data.findIndex(
+            (project) => project.uuid == mainProjectId
+        );
+        if (mainProjectIndex < 0) {
+            window.location.href = "../pages/404.html";
         }
-      }`,
-      variables: { postId },
-    }),
-  });
-  const json = await response.json();
-  return json.data.post;
+
+        const mainProject = data[mainProjectIndex];
+        addMainProject(mainProject);
+
+        data.splice(mainProjectIndex, 1);
+        const otherProjects = data.slice(0, 3).reverse();
+        addOtherProjects(otherProjects);
+    } catch (error) {
+        toggleModal();
+    }
+});
+
+function getQuerystringId() {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+    });
+
+    return params.id;
 }
 
-export async function loadPost() {
-  const postId = parseInt(
-    new URLSearchParams(window.location.search).get("id")
-  );
-  const postContent = await getPostById(postId);
-  if (postContent) {
-    setTitle(postContent.title);
-    setDescription(postContent.description);
-    setBody(postContent.body);
-    setCover(postContent.cover, postContent.coverDescription);
-    setCreationDate(new Date(postContent.createdAt));
-    return postContent;
-  }
+function addMainProject(project) {
+    if (!project) {
+        return;
+    }
+
+    const projectHTML = `
+          <h1 class="title">${project.name}</h1>
+          <div class="subtitle">
+          <span class="UI-design-title">${project.description}</span>
+  
+          <span class="completed-title">Completed on
+              <span class="completed-title-data">${project.completed_on}</span>
+          </span>
+          </div>
+          <div class="project-image-section">
+          <img class="project-image" src="${project.image}" alt="${project.name} image" />
+          </div>
+  
+          <article class="project-description">
+              ${project.content}
+          </article>
+      `;
+
+    const projectElement = document.getElementById("project");
+    projectElement.innerHTML = projectHTML;
 }
 
-const setTitle = (title: string) => {
-  document.querySelector(".post-section__post-title").innerHTML = title;
-}
-const setDescription = (description: string) => {
-  document.querySelector(
-    ".post-section__post-subtitle__description"
-  ).innerHTML = description;
-}
-const setBody = (body: string) => {
-  document.querySelector(".post-section__text").innerHTML = body;
-}
-const setCover = (coverImage: string, coverDescription: string) => {
-  const foreFrontImage = document.querySelector(
-    ".post-section__image__forefront-image"
-  ) as HTMLImageElement
-  foreFrontImage.src = coverImage;
-  foreFrontImage.alt = coverDescription;
+function addOtherProjects(projects) {
+    let articlesHTML = "";
 
-  const backgroundImage = document.querySelector(
-    ".post-section__image__background-image"
-  ) as HTMLImageElement
+    projects.forEach((project) => {
+        articlesHTML += jsonProjectToOtherHtmlArticle(project);
+    });
 
-  backgroundImage.src = coverImage;
-  backgroundImage.alt = coverDescription;
+    const container = document.querySelector("div.projects-container");
+    container.innerHTML = articlesHTML;
 }
 
-const setCreationDate = (date: Date) => {
-  document.querySelector(
-    ".post-section__post-subtitle__date__value"
-  ).innerHTML = date.toDateString();
-}
+function jsonProjectToOtherHtmlArticle(project) {
+    if (!project) {
+        return;
+    }
 
-window.addEventListener("load", loadPost);
+    const projectHTML = `
+          <article class="project-card">
+              <a class="project-wrapper" href="../pages/projects.html?id=${project.uuid}">
+                  <img class="img-project" src="${project.image}" alt="${project.name} image" />
+                  <div class="project-inner-card">
+                  <h4 class="project-title">${project.name}</h4>
+                  <p class="project-description capitalize">${project.description}</p>
+                  <a class="learn-more" href="../pages/projects.html?id=${project.uuid}">Learn more</a>
+                  </div>
+              </a>
+          </article>
+      `;
+
+    return projectHTML;
+}
